56f7123bfb32de215159c14db2cdf420fab0d4ac
Joell <amamihechi.udu@decagon.dev>
2020-01-12 06:53:05 +0100

    Add project files.

---
+++ c-sharp with mosh/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c_sharp_with_mosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person mypPerson = new Person();
+            mypPerson.firstName = "james";
+            mypPerson.lastName = "rom";
+            mypPerson.Introduce(mypPerson.lastName);
+        }
+    }
+
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce(string name)
+        {
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
---
+++ c-sharp with mosh/helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace c_sharp_with_mosh
+{
+    class helper
+    {
+        public class Calculator
+        {
+            public static int Add(int firstNum, int secNum)
+            {
+                return firstNum + secNum;
+            }
+        }
+    }
+}
