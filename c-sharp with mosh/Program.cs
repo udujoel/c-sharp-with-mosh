359943e14208ba6bf85f8ad4da1bff7893947337
Joell <amamihechi.udu@decagon.dev>
2020-02-09 00:37:57 +0100

    verbatim

--- c-sharp with mosh/Program.cs
+++ c-sharp with mosh/Program.cs
@@ -2,6 +2,12 @@
 
 namespace c_sharp_with_mosh
 {
+    public enum Currency
+    {
+        Naira = 1,
+        Dollar = 2,
+        Euro = 3
+    }
     class Program
     {
         static void Main(string[] args)
@@ -19,14 +25,22 @@ namespace c_sharp_with_mosh
             int count = 0;
             while (count < list.Length)
             {
-                Console.WriteLine(list[count]);
+                //Console.WriteLine(list[count]);
                 count++;
             }
 
             Console.WriteLine(list);
 
-            string path = @"c:\windows\system/anywhereIwant";
-            Console.WriteLine(path);
+            //string path = @"c:\windows\system/anywhereIwant";
+            //Console.WriteLine(path);
+
+            var amountSign = Currency.Naira;
+            Console.WriteLine(amountSign);
+            Console.WriteLine((int)amountSign);
+            
+            var sign = 2;
+            Console.WriteLine((Currency)sign);
+
         }
     }
 }
